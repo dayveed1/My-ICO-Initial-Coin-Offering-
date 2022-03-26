bc168cd9e30aef8f29c93713b8093fa19926e9d8
david <dayveed1@users.noreply.github.com>
2022-03-26 10:38:40 +0100

    My first push to ICO since i had to restart my learning  cuz i met with an error

---
+++ Hardhat-Tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x55d1d64C9fAEcbDB69a10F8C33322578BdeB4d72";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
